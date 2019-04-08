26c247298c9965144ae51edb1cbf17c4bfdce27f
Stephan Avenwedde <s.avenwedde@gmail.com>
2019-04-08 22:22:34 +0200

    Despelled unnecessary files

--- pythonic-binance/__init__.py
+++ pythonic-binance/__init__.py
@@ -1,5 +1,6 @@
 """An unofficial Python wrapper for the Binance exchange API v3
 
 .. moduleauthor:: Sam McHardy
+.. Modified by Stephan Avenwedde for Pythonic
 
 """
--- pythonic-binance/client.py
+++ pythonic-binance/client.py
@@ -5,8 +5,8 @@ import hmac
 import requests
 import time
 from operator import itemgetter
-from .helpers import date_to_milliseconds, interval_to_milliseconds
-from .exceptions import BinanceAPIException, BinanceRequestException, BinanceWithdrawException
+from helpers import date_to_milliseconds, interval_to_milliseconds
+from exceptions import BinanceAPIException, BinanceRequestException, BinanceWithdrawException
 
 
 class Client(object):
